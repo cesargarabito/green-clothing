a2f28ad0a5ae28b81d2761d820c7f9a34f2c3f08
cesargarabito <cesargarabito97@gmail.com>
2022-11-16 18:58:30 +0100

    add items to cart built

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,13 +4,13 @@ import { CartContext } from '../../contexts/cart.context';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg'
 
 const CartIcon = () => {
-    const { isOpen, setIsOpen } = useContext(CartContext);
+    const { isOpen, setIsOpen, cartCount } = useContext(CartContext);
 
     const toggleIsOpen = () => setIsOpen(!isOpen);
     return (
 <div className='cart-icon-container'>
 <ShoppingIcon className='shopping-icon' onClick={toggleIsOpen} />
-<span className='item-count'>0</span>
+<span className='item-count'>{cartCount}</span>
 </div>
     );
 }
